d0214dcf7c9845c64b8e99ab680955af8783e564
DESKTOP-D314UU8\khangle2307 <khangle2307@gmail.com>
2023-12-08 19:02:18 +0700

    feat: delete file

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import cloudinary from "../config/cloudinary.config";
 
 export const uploadFile = (req: Request, res: Response) => {
   try {
@@ -10,6 +11,23 @@ export const uploadFile = (req: Request, res: Response) => {
         url: image?.path,
         public_id: image?.filename
       }
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: true,
+      message: error
+    })
+  }
+}
+
+export const deleteFile = async (req: Request, res: Response) => {
+  try {
+    const { public_id } = req.body;
+    await cloudinary.uploader.destroy(public_id);
+    return res.status(200).json({
+      status: 200,
+      message: "Xóa ảnh thành công",
     })
   } catch (error) {
     return res.status(500).json({
--- src/routes/v1/upload.route.ts
+++ src/routes/v1/upload.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { upload } from "../../config/upload.config";
-import { uploadFile } from "../../controllers/upload.controller";
+import { deleteFile, uploadFile } from "../../controllers/upload.controller";
 
 const route = Router()
 route.post('/upload', upload.single('file'), uploadFile)
+route.post('/destroy', deleteFile)
 
 export default route;
